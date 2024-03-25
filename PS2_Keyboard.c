32376c919af5732902e51a885a846bcef997f933
Eshal Arshad <79858502+EshalArshad@users.noreply.github.com>
2024-03-24 23:18:33 -0400

    Update PS2_Keyboard.c

--- PS2_Keyboard.c
+++ PS2_Keyboard.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdbool.h>
 
 volatile int* ps2_ptr = (int*) 0xFF200100; // Pointer to PS2
@@ -7,97 +8,52 @@ void handleKeyboardInput(char *word) {
     int keyData;
     bool enterPressed = false;
     int wordIndex = 0;
+    bool keyReleased = true; // Track if the last key was released
 
     while (!enterPressed) {
         keyData = *ps2_ptr;
-        int key = keyData & 0xFF; // Extract the lower 8 bits
-
-        // Check if any of the alphabet letters (A-Z) are pressed
-        if ((keyData & 0xFF) == 0x1C) { // A
-            word[wordIndex++] = 'A';
-            printf("Letter A added\n");
-        } else if ((keyData & 0xFF) == 0x32) { // B
-            word[wordIndex++] = 'B';
-            printf("Letter B added\n");
-        } else if ((keyData & 0xFF) == 0x21) { // C
-            word[wordIndex++] = 'C';
-            printf("Letter C added\n");
-        } else if ((keyData & 0xFF) == 0x23) { // D
-            word[wordIndex++] = 'D';
-            printf("Letter D added\n");
-        } else if ((keyData & 0xFF) == 0x24) { // E
-            word[wordIndex++] = 'E';
-            printf("Letter E added\n");
-        } else if ((keyData & 0xFF) == 0x2B) { // F
-            word[wordIndex++] = 'F';
-            printf("Letter F added\n");
-        } else if ((keyData & 0xFF) == 0x34) { // G
-            word[wordIndex++] = 'G';
-            printf("Letter G added\n");
-        } else if ((keyData & 0xFF) == 0x33) { // H
-            word[wordIndex++] = 'H';
-            printf("Letter H added\n");
-        } else if ((keyData & 0xFF) == 0x43) { // I
-            word[wordIndex++] = 'I';
-            printf("Letter I added\n");
-        } else if ((keyData & 0xFF) == 0x3B) { // J
-            word[wordIndex++] = 'J';
-            printf("Letter J added\n");
-        } else if ((keyData & 0xFF) == 0x42) { // K
-            word[wordIndex++] = 'K';
-            printf("Letter K added\n");
-        } else if ((keyData & 0xFF) == 0x4B) { // L
-            word[wordIndex++] = 'L';
-            printf("Letter L added\n");
-        } else if ((keyData & 0xFF) == 0x3A) { // M
-            word[wordIndex++] = 'M';
-            printf("Letter M added\n");
-        } else if ((keyData & 0xFF) == 0x31) { // N
-            word[wordIndex++] = 'N';
-            printf("Letter N added\n");
-        } else if ((keyData & 0xFF) == 0x44) { // O
-            word[wordIndex++] = 'O';
-            printf("Letter O added\n");
-        } else if ((keyData & 0xFF) == 0x4D) { // P
-            word[wordIndex++] = 'P';
-            printf("Letter P added\n");
-        } else if ((keyData & 0xFF) == 0x15) { // Q
-            word[wordIndex++] = 'Q';
-            printf("Letter Q added\n");
-        } else if ((keyData & 0xFF) == 0x2D) { // R
-            word[wordIndex++] = 'R';
-            printf("Letter R added\n");
-        } else if ((keyData & 0xFF) == 0x1B) { // S
-            word[wordIndex++] = 'S';
-            printf("Letter S added\n");
-        } else if ((keyData & 0xFF) == 0x2C) { // T
-            word[wordIndex++] = 'T';
-            printf("Letter T added\n");
-        } else if ((keyData & 0xFF) == 0x3C) { // U
-            word[wordIndex++] = 'U';
-            printf("Letter U added\n");
-        } else if ((keyData & 0xFF) == 0x2A) { // V
-            word[wordIndex++] = 'V';
-            printf("Letter V added\n");
-        } else if ((keyData & 0xFF) == 0x1D) { // W
-            word[wordIndex++] = 'W';
-            printf("Letter W added\n");
-        } else if ((keyData & 0xFF) == 0x22) { // X
-            word[wordIndex++] = 'X';
-            printf("Letter X added\n");
-        } else if ((keyData & 0xFF) == 0x35) { // Y
-            word[wordIndex++] = 'Y';
-            printf("Letter Y added\n");
-        } else if ((keyData & 0xFF) == 0x1A) { // Z
-            word[wordIndex++] = 'Z';
-            printf("Letter Z added\n");
-        } else if ((keyData & 0xFF) == 0x1C) { // Enter
-            enterPressed = true;
+        if (keyData != 0) { // Check if data is available
+            int key = keyData & 0xFF; // Extract the lower 8 bits
+            
+            if (key == 0xF0) { // Key release code
+                keyReleased = true; // Next key data will be a key release
+            } else if (keyReleased) { // Process key press only if the previous key was released
+                switch (key) {
+                    case 0x1C: word[wordIndex++] = 'A'; printf("Letter A added\n"); break;
+                    case 0x32: word[wordIndex++] = 'B'; printf("Letter B added\n"); break;
+                    case 0x21: word[wordIndex++] = 'C'; printf("Letter C added\n"); break;
+                    case 0x23: word[wordIndex++] = 'D'; printf("Letter D added\n"); break;
+                    case 0x24: word[wordIndex++] = 'E'; printf("Letter E added\n"); break;
+                    case 0x2B: word[wordIndex++] = 'F'; printf("Letter F added\n"); break;
+                    case 0x34: word[wordIndex++] = 'G'; printf("Letter G added\n"); break;
+                    case 0x33: word[wordIndex++] = 'H'; printf("Letter H added\n"); break;
+                    case 0x43: word[wordIndex++] = 'I'; printf("Letter I added\n"); break;
+                    case 0x3B: word[wordIndex++] = 'J'; printf("Letter J added\n"); break;
+                    case 0x42: word[wordIndex++] = 'K'; printf("Letter K added\n"); break;
+                    case 0x4B: word[wordIndex++] = 'L'; printf("Letter L added\n"); break;
+                    case 0x3A: word[wordIndex++] = 'M'; printf("Letter M added\n"); break;
+                    case 0x31: word[wordIndex++] = 'N'; printf("Letter N added\n"); break;
+                    case 0x44: word[wordIndex++] = 'O'; printf("Letter O added\n"); break;
+                    case 0x4D: word[wordIndex++] = 'P'; printf("Letter P added\n"); break;
+                    case 0x15: word[wordIndex++] = 'Q'; printf("Letter Q added\n"); break;
+                    case 0x2D: word[wordIndex++] = 'R'; printf("Letter R added\n"); break;
+                    case 0x1B: word[wordIndex++] = 'S'; printf("Letter S added\n"); break;
+                    case 0x2C: word[wordIndex++] = 'T'; printf("Letter T added\n"); break;
+                    case 0x3C: word[wordIndex++] = 'U'; printf("Letter U added\n"); break;
+                    case 0x2A: word[wordIndex++] = 'V'; printf("Letter V added\n"); break;
+                    case 0x1D: word[wordIndex++] = 'W'; printf("Letter W added\n"); break;
+                    case 0x22: word[wordIndex++] = 'X'; printf("Letter X added\n"); break;
+                    case 0x35: word[wordIndex++] = 'Y'; printf("Letter Y added\n"); break;
+                    case 0x1A: word[wordIndex++] = 'Z'; printf("Letter Z added\n"); break;
+                    case 0x5A: enterPressed = true; break; // Enter key pressed
+                    // No default case needed as we only care about the keys above
+                }
+                keyReleased = false; // Reset for next key press
+            }
         }
     }
 
-    //idk if needed - need to testtttttttttt
-    word[wordIndex] = '\0';
+    word[wordIndex] = '\0'; // Null-terminate the string
 }
 
 int main() {
